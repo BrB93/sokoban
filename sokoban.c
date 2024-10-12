6d495faafadf769e1e3e55fd324ff9740f687e32
Brb93 <bryanbrik@gmail.com>
2024-10-12 08:20:06 +0200

    ajout des emplacements aléatoires

--- sokoban.c
+++ sokoban.c
@@ -7,7 +7,7 @@
 #define SIZE 10
 
 
-char ** init_board(int pos_i, int pos_j)
+char ** init_board(int pos_i, int pos_j, int pos_ca, int pos_cb, int pos_ga, int pos_gb)
 {
 	char ** board = malloc(SIZE * sizeof(char *));
 	for (int i = 0; i < SIZE; i++)
@@ -24,7 +24,9 @@ char ** init_board(int pos_i, int pos_j)
 		}	
 	}
 	board[pos_i][pos_j] = 'o';
-
+	board[pos_ca][pos_cb] = 'X';
+	board[pos_ga][pos_gb] = '.';
+	
 
 	return board;
 
--- sokoban.h
+++ sokoban.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <time.h>
 
-char ** init_board(int pos_i, int pos_j);
+char ** init_board(int pos_i, int pos_j, int pos_ca, int pos_cb, int pos_ga, int pos_gb);
 void display_board(char **board);
 
 
